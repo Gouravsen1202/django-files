4592d20b60a9febb17efb44604f943fe32b25dde
gouravsen1202 <gouravsen757@gmail.com>
2025-05-04 23:33:45 +0530

    new code

--- django/Property Bazar Poject/restronent/myapp/urls.py
+++ django/Property Bazar Poject/restronent/myapp/urls.py
@@ -4,14 +4,21 @@ from myapp import views
 
 urlpatterns = [
 path('',views.home,name='home'),
-path('',views.home1,name='home'),
 path('menu/',views.menu,name='menu'), #selling
 path('order/',views.order,name='order'),#purchase
-path('menu/',views.menu1,name='menu'), #selling
-path('order/',views.order1,name='order'),#purchase
 path('Ragister/',views.ragister,name='Ragister'),
 path('login/',views.login,name='login'),
 path('deshbord/',views.deshbord,name='deshbord'),
+path('logout/', views.logout, name='logout'),
 path('track/',views.trackorder,name='track order'),
-path('track/',views.trackorder1,name='track order'),
+path('Service/',views.Service,name='Service'),
+
+
+path('home/<int:pk>',views.home1,name='home1'),
+path('menu/<int:pk>',views.menu1,name='menu1'),
+path('order/<int:pk>',views.order1,name='order1'),
+path('track/<int:pk>',views.trackorder1,name='track order1'),
+path('Service/<int:pk>',views.Service1,name='Service1')
+
+
 ]
--- django/Property Bazar Poject/restronent/myapp/views.py
+++ django/Property Bazar Poject/restronent/myapp/views.py
@@ -4,8 +4,7 @@ from .models import Bazzar
 # Create your views here.
 def home(request):
     return render(request,'myapp/home.html')
-def home1(request):
-    return render(request,'myapp/home.html')
+
 
 
 def common(request):
@@ -14,22 +13,17 @@ def common(request):
 
 def menu(request):#selling
     return render(request,'myapp/selling.html')
-def menu1(request):
-    return render(request,'myapp/selling.html')
+
 
 def order(request):#purchase
     return render(request,'myapp/purchase.html')
-def order1(request):
-    return render(request,'myapp/purchase.html')
 
-def deshbord(request):
-    return render(request,'myapp/desbord.html')
+
 
 def trackorder(request):
     return render(request,'myapp/track.html')
-
-def trackorder1(request):
-    return render(request,'myapp/track.html')
+def Service(request):
+    return render(request,'myapp/Service.html')
 
 # for login data 
 
@@ -83,13 +77,46 @@ def login(request):
            print(userdata.emp_email)
            p1=userdata.emp_password
            if p==p1:
-               return render(request,'myapp/deshbord.html',{'userdata':userdata})
+               msg="login succcessfuly"
+               return render(request,'myapp/deshbord.html',{'userdata':userdata,'msg':msg,'msg_type': 'success'})
            else:
                msg="password not match"
                return render(request,'myapp/login.html',{'email':e})
        else:
              msg="email does not exist"
-             return render(request,'myappp/ragister.html')
+             return render(request,'myapp/ragister.html')
        
     else:
         return render(request,'myapp/login.html')
+
+def home1(request,pk):
+    userdata=Bazzar.objects.get(id=pk)
+    return render(request,'myapp/home.html',{'userdata':userdata})
+
+def menu1(request,pk):
+    userdata=Bazzar.objects.get(id=pk)
+    return render(request,'myapp/selling.html',{'userdata':userdata})
+
+def order1(request,pk):
+    userdata=Bazzar.objects.get(id=pk)
+    return render(request,'myapp/purchase.html',{'userdata':userdata})
+
+def trackorder1(request,pk):
+    userdata=Bazzar.objects.get(id=pk)
+    return render(request,'myapp/track.html',{'userdata':userdata})
+
+def Service1(request,pk):
+    userdata=Bazzar.objects.get(id=pk)
+    return render(request,'myapp/Service.html',{'userdata':userdata})
+
+
+
+def deshbord(request):
+    return render(request,'myapp/deshbord.html')
+
+def logout(request):
+    return render(request,'myapp/login.html')
+
+   
+
+
